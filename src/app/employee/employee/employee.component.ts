afecbf18a0db5ffac569c90cad40af27950a7beb
Abdullah Alrookan <aalrookan@Abdullahs-MacBook-Pro.local>
2022-06-29 21:55:06 +0300

    worked on employees and still working on it

--- src/app/employee/employee/employee.component.ts
+++ src/app/employee/employee/employee.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { profile } from 'console';
+import { EmployeeService } from './employee.service';
 
 @Component({
   selector: 'app-employee',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeComponent implements OnInit {
 
-  constructor() { }
+  employeesList: any = []
+  //employeeProfile: profiles = []
+
+  constructor(
+    private http: HttpClient,
+    public employeesWorker: EmployeeService
+    //this.employeeProfile.push(new profiles());
+    ) { }
 
   ngOnInit(): void {
+    this.employeesWorker.getAllEmployees().subscribe(
+      (result:any)=> {
+        this.employeesList = result
+      }
+    );
+    //console.log(this.employeesList)
   }
 
+  /*add() {
+    this.employeeProfile.push(new profiles())
+  }*/
+
 }
+
+/*export class profiles{
+  first_name: string | undefined;
+  last_name: string | undefined;
+  email: string | undefined; 
+  active: string | undefined; 
+}*/
